64c0cfc9ec8afa592328d08b5b4b8e312dc39aff
Michal Rogowski <michrogowski@gmail.com>
2019-11-17 15:17:25 +0100

    Initial Xcode project.

---
+++ Videolitic/Videolitic/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Videolitic
+//
+//  Created by Michał Rogowski on 17/11/2019.
+//  Copyright © 2019 Michał Rogowski. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
